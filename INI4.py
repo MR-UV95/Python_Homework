8ed1552faf7ab0e7ee6f09c7700977435bed9ffb
MR-UV95 <rahmanmahmudabir@gmail.com>
2024-11-15 10:03:06 +0100

    Fixed the code

--- INI4.py
+++ INI4.py
@@ -5,9 +5,10 @@
 #Code
 ## Given numbers are 4390, 9233
 {
-    odd = 0
+odd = 0
 for num in range(4390, 9234):
     if num % 2 == 1:
             odd = odd + num
-    print (odd)
+    
+print (odd)
 }
